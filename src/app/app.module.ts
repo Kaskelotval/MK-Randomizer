5a7addbdfadd66474391319b69f07004021251ad
Jens Kaske <jensk@axis.com>
2019-09-27 11:02:14 +0200

    mega commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { RandomizerComponent } from "./randomizer/randomizer.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, RandomizerComponent],
+  imports: [BrowserModule, AppRoutingModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/randomizer/randomizer.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from "@angular/core";
+import * as data from "./data.json";
+
+@Component({
+  selector: "app-randomizer",
+  templateUrl: "./randomizer.component.html",
+  styleUrls: ["./randomizer.component.css"]
+})
+export class RandomizerComponent implements OnInit {
+  constructor() {}
+
+  private readonly tournaments = data.tournaments;
+  private readonly tracksToPlay = 4;
+
+  private tournamentAmount = 0;
+  private tracksTotal = 0;
+  private chosenTracks = [];
+
+  public mobile = false;
+
+  ngOnInit() {
+    this.tournamentAmount = this.tournaments.length;
+    this.tracksTotal = this.tournaments
+      .map(tournament => {
+        return tournament.tracks.length;
+      })
+      .reduce((x, y) => {
+        return x + y;
+      });
+
+    if (window.screen.width <= 700) {
+      this.mobile = true;
+    }
+  }
+  onResize() {
+    if (window.screen.width <= 700) {
+      this.mobile = true;
+    } else {
+      this.mobile = false;
+    }
+  }
+  public getListOfTracks() {
+    return this.chosenTracks;
+  }
+
+  public getTournament(tournamentIdx) {
+    return this.tournaments[tournamentIdx];
+  }
+
+  public getTrack(tournamentIdx, trackIdx) {
+    return this.tournaments[tournamentIdx].tracks[trackIdx];
+  }
+
+  public getImageByName(name: string) {
+    return "../../assets/images/" + name.replace(/ /g, "_") + ".png";
+  }
+  private getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  public getRandomTracks() {
+    this.chosenTracks = [];
+    let tournamentNumber;
+    let trackNumber;
+    let trackObject;
+
+    do {
+      tournamentNumber = this.getRandomInt(this.tournamentAmount);
+      trackNumber = this.getRandomInt(this.getRandomInt(4));
+      trackObject = {
+        tournamentNumber: tournamentNumber,
+        trackNumber: trackNumber
+      };
+      if (
+        this.chosenTracks.find(
+          obj =>
+            obj.tournamentNumber == trackObject.tournamentNumber &&
+            obj.trackNumber == trackObject.trackNumber
+        )
+      ) {
+      } else {
+        this.chosenTracks.push(trackObject);
+      }
+    } while (this.chosenTracks.length < this.tracksToPlay);
+  }
+}
